8165f95f791ce646035987db4ff79fe9a7c97e3d
Shyam-12 <shyamprasad1248@gmail.com>
2023-07-15 01:01:38 +0530

    updated homepage
    Signed-off-by: Shyam-12 <shyamprasad1248@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,99 +1,54 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
+import { useState } from "react";
 import "./styles/Home.css";
+import Main from './components/Main';
 
 export default function Home() {
+
+  const [loggedIn, setLoggedIn] = useState(false);
+  // const [userProfile, setUserProfile] = useState(null);
+
+  const handleLogin = () => {
+    // Simulating a login action with a delay
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
   return (
-    <main className="main">
-      <div className="container">
-        <div className="header">
-          <h1 className="title">
-            Welcome to{" "}
-            <span className="gradient-text-0">
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
-
-          <p className="description">
-            Get started by configuring your desired network in{" "}
-            <code className="code">src/index.js</code>, then modify the{" "}
-            <code className="code">src/App.js</code> file!
-          </p>
-
-          <div className="connect">
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
-              }}
-            />
+    <div>
+      <main className="main">
+        <div className="container">
+          <div className="header">
+              <header>
+                <img src="/images/logo.jpg" alt="thirdweb logo" />
+              </header>
+              <div className="user-login">
+                {loggedIn ? (
+                    <div>
+                      <div className="connect">
+                        <ConnectWallet
+                          dropdownPosition={{
+                            side: "bottom",
+                            align: "center",
+                          }}
+                        />
+                      </div>
+                      <button onClick={handleLogout}>Logout</button>
+                    </div>
+                  ) : (
+                    <div>
+                      <button onClick={handleLogin}>Login</button>
+                      <button>Sign Up</button>
+                    </div>
+                  )}   
+              </div>
           </div>
         </div>
-
-        <div className="grid">
-          <a
-            href="https://portal.thirdweb.com/"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-1">Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/dashboard"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-2">Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
-
-          <a
-            href="https://thirdweb.com/templates"
-            className="card"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-            />
-            <div className="card-text">
-              <h2 className="gradient-text-3">Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
-      </div>
-    </main>
+      </main>
+      <Main />
+    </div>
   );
 }
